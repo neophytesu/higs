56311e1d54a37884b4c6da98ce0c1474892411ac
86158 <2410973613@qq.com>
2024-08-26 20:51:25 +0800

    初始

---
+++ higs-service/src/main/java/com/su/HigsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.su;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HigsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HigsServiceApplication.class, args);
+    }
+
+}
---
+++ higs-service/src/test/java/com/su/HigsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.su;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HigsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
